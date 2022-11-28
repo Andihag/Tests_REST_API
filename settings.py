922eabf7f81525581a24f3beed430a907e414079
Andyhag <a-atyakshev56@yandex.ru>
2022-11-28 21:34:02 +0500

    add tests

---
+++ settings.py
@@ -0,0 +1,6 @@
+valid_email = "a-atyakshev56@yandex.ru"
+valid_password = "Kr54,.kt"
+empty_email = ""
+empty_password = ""
+empty_auth_key = {"key": ""}
+incorrect_auth_key = {"key": "000000000000000000000000000000000000000000000"}
